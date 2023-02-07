6b867f58db5f3e3f5fb1201f6231ee184a9a2788
Pedro Teixeira <p3drinoh@gmail.com>
2023-02-07 13:14:56 -0300

    Updates

---
+++ precache-manifest.9d7a80d3569b2df552cb2b8fcf8f053d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fcae1fb4b60e95ebad4",
+    "url": "/static/css/main.eb24c9f1.chunk.css"
+  },
+  {
+    "revision": "5fcae1fb4b60e95ebad4",
+    "url": "/static/js/main.5fcae1fb.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1a12774cd825b4be1675099d884f292d",
+    "url": "/index.html"
+  }
+];
